4183bb94ce7e811939f2fb9ee14a1ee36dae60fe
MoAlyousef <may642_2000@hotmail.com>
2022-04-03 16:22:02 +0300

    remove old ex

--- examples/ex.rs
+++
@@ -1,8 +0,0 @@
-use livid::{Event, Style, Widget, WidgetType};
-
-fn main() {
-    let div = Widget::new(WidgetType::Div);
-    div.set_text_content(Some("Hello"));
-    div.add_callback(Event::Click, |d| d.set_text_content(Some("Clicked")));
-    div.set_style(Style::BackgroundColor, "red");
-}
