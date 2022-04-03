7a91a9a95f4b264f4b215723b33b05c787333d94
MoAlyousef <may642_2000@hotmail.com>
2022-04-03 17:38:19 +0300

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # livid-rs
+
+livid is a lightweight frontend Rust crate.
+
+## Requirements
+- The wasm32-unknown-unknown target:
+- `rustup target add wasm32-unknown-unknown`
+
+## Usage
+- Install the trunk crate (to simplify building and bundling your web app):
+- `cargo install trunk`
+
+- Create an index.html file in your root directory:
+```html
+<html>
+  <head>
+  </head>
+</html>
+```
+* You can add links to CSS files/urls, and use Widget::set_class_name() to benefit from CSS styling.
+
+- Create a project:
+```toml
+[dependencies]
+livid = { git = "https://github.com/MoAlyousef/livid-rs" }
+```
+
+```rust
+use livid::{Event, Style, Widget, WidgetType, document};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+fn main() {
+    let count = Rc::from(RefCell::from(0));
+
+    let btn_inc = Widget::new(WidgetType::Button);
+    btn_inc.set_text_content(Some("Increment"));
+    btn_inc.set_style(Style::Color, "green");
+    btn_inc.add_callback(Event::Click, {
+        let cnt = count.clone();
+        move |_| {
+            *cnt.borrow_mut() += 1;
+            let result = document().get_element_by_id("result").unwrap();
+            result.set_text_content(Some(&cnt.borrow().to_string()));
+    }});
+
+    let div = Widget::new(WidgetType::Div);
+    div.append_child(&btn_inc).unwrap();
+
+    let result = Widget::new(WidgetType::Div);
+    result.set_id("result");
+    result.set_text_content(Some("0"));
+    result.set_style(Style::FontSize, "22px");
+}
+```
+
+- Build and serve using Trunk:
+- `trunk build`
+- `trunk serve`
---
+++ examples/misc/src/main.rs
@@ -0,0 +1,88 @@
+use livid::{Style, Widget, WidgetType};
+
+fn ul() -> Widget {
+    Widget::new(WidgetType::Ul)
+}
+
+fn li() -> Widget {
+    Widget::new(WidgetType::Li)
+}
+
+fn svg() -> Widget {
+    Widget::new(WidgetType::Svg)
+}
+
+fn div() -> Widget {
+    Widget::new(WidgetType::Div)
+}
+
+fn table() -> Widget {
+    Widget::new(WidgetType::Table)
+}
+
+fn tbody() -> Widget {
+    Widget::new(WidgetType::Tbody)
+}
+
+fn tr() -> Widget {
+    Widget::new(WidgetType::Tr)
+}
+
+fn td() -> Widget {
+    Widget::new(WidgetType::Td)
+}
+
+fn create_table(data: &Vec<Vec<i32>>) -> Widget {
+    let t = table();
+    t.set_attribute("border", "1").unwrap();
+    let tb = tbody();
+    t.append_child(&*tb).unwrap();
+    for i in 0..data.len() {
+        let tr = tr();
+        tb.append_child(&*tr).unwrap();
+
+        for j in 0..data[i].len() {
+            let td = td();
+            td.set_attribute("width", "100").unwrap();
+            td.set_text_content(Some(&data[i][j].to_string()));
+            tr.append_child(&*td).unwrap();
+        }
+    }
+    t
+}
+
+fn main() {
+    let ul = ul();
+    for i in 0..10 {
+        let li = li();
+        li.set_text_content(Some(&i.to_string()));
+        ul.append_child(&li).unwrap();
+    }
+
+    let svg = svg();
+    svg.set_attribute("viewBox", "0 0 300 300").unwrap();
+    svg.set_attribute("width", "300").unwrap();
+    svg.set_attribute("height", "300").unwrap();
+    svg.set_style(Style::Display, "block");
+    svg.set_inner_html(
+        r#"
+        <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red"></circle>
+    "#,
+    );
+
+    let div = div();
+    div.set_outer_html(
+        r#"
+        <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="50" cy="50" r="50"/>
+        </svg>    
+        "#,
+    );
+
+    let data = vec![
+        vec![1, 2, 3, 4, 5, 6, 7],
+        vec![10, 11, 12, 13, 14, 15, 16]
+    ];
+
+    create_table(&data);
+}
