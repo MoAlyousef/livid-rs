d61abd43c56e1ea821ab8bf26653daba54b82a10
MoAlyousef <may642_2000@hotmail.com>
2022-04-17 12:15:26 +0300

    return from rt call

--- livid/src/rt.rs
+++ livid/src/rt.rs
@@ -1,14 +1,14 @@
 use wasm_bindgen::prelude::*;
 use js_sys::Reflect;
 
-pub fn call(method: &str, args: &[&str]) {
+pub fn call(method: &str, args: &[&str]) -> Result<JsValue, JsValue> {
     let obj = web_sys::window().unwrap();
     let method: js_sys::Function = Reflect::get(&obj, &JsValue::from_str(method)).unwrap().into();
     let arguments = js_sys::Array::new();
     for arg in args {
         arguments.push(&JsValue::from_str(arg));
     }
-    Reflect::apply(&method, &obj, &arguments).unwrap();
+    Reflect::apply(&method, &obj, &arguments)
 }
 
 pub fn alert(msg: &str) {
