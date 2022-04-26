2113b4a9e2127054996cf117e9fe982ec5a291bf
MoAlyousef <may642_2000@hotmail.com>
2022-04-26 16:30:39 +0300

    further cleanup

--- examples/counter-elmish/src/detail.rs
+++ examples/counter-elmish/src/detail.rs
@@ -1,6 +1,84 @@
 use livid::{enums::*, prelude::*, *};
+use std::any::Any;
 use std::cell::RefCell;
+use std::marker;
 use std::rc::Rc;
+
+lazy_static::lazy_static! {
+    static ref CHANNEL: (crossbeam_channel::Sender<Box<dyn Any + Send + Sync>>, crossbeam_channel::Receiver<Box<dyn Any + Send + Sync>>) = crossbeam_channel::unbounded();
+    static ref SENDER: crossbeam_channel::Sender<Box<dyn Any + Send + Sync>> = CHANNEL.clone().0;
+    static ref RECEIVER: crossbeam_channel::Receiver<Box<dyn Any + Send + Sync>> = CHANNEL.clone().1;
+}
+
+/// Creates a sender struct
+#[derive(Debug, Copy)]
+pub struct Sender<T: Send + Sync> {
+    data: marker::PhantomData<T>,
+}
+
+unsafe impl<T: Send + Sync> Send for Sender<T> {}
+unsafe impl<T: Send + Sync> Sync for Sender<T> {}
+
+// Manually create the impl so there's no Clone bound on T
+impl<T: Send + Sync> Clone for Sender<T> {
+    fn clone(&self) -> Self {
+        Sender {
+            data: marker::PhantomData,
+        }
+    }
+}
+
+impl<T: 'static + Send + Sync> Sender<T> {
+    /// Sends a message
+    pub fn send(&self, val: T) {
+        SENDER.try_send(Box::new(val)).ok();
+    }
+}
+
+/// Creates a receiver struct
+#[derive(Debug, Copy)]
+pub struct Receiver<T: Send + Sync> {
+    data: marker::PhantomData<T>,
+}
+
+unsafe impl<T: Send + Sync> Send for Receiver<T> {}
+unsafe impl<T: Send + Sync> Sync for Receiver<T> {}
+
+// Manually create the impl so there's no Clone bound on T
+impl<T: Send + Sync> Clone for Receiver<T> {
+    fn clone(&self) -> Self {
+        Receiver {
+            data: marker::PhantomData,
+        }
+    }
+}
+
+impl<T: 'static + Send + Sync> Receiver<T> {
+    /// Receives a message
+    pub fn recv(&self) -> Option<T> {
+        // if let Some(r) = &*RECEIVER {
+        if let Ok(msg) = RECEIVER.try_recv() {
+            if let Ok(message) = msg.downcast() {
+                Some(*message)
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+}
+
+/// Creates a channel returning a Sender and Receiver structs (mpsc: multiple producer single consumer).
+pub fn channel<T: Send + Sync>() -> (Sender<T>, Receiver<T>) {
+    let s = Sender {
+        data: marker::PhantomData,
+    };
+    let r = Receiver {
+        data: marker::PhantomData,
+    };
+    (s, r)
+}
 
 pub trait OnEvent<T>
 where
@@ -16,7 +94,7 @@ where
     T: Send + Sync + Clone + 'static,
 {
     fn on_trigger(self, msg: T) -> Self {
-        let (s, _) = document::channel::<T>();
+        let (s, _) = channel::<T>();
         self.add_callback(Event::Click, move |_| {
             s.send(msg.clone());
         });
@@ -51,7 +129,7 @@ pub trait App: Clone {
         }
         self.view();
         win.end();
-        let (_, r) = document::channel::<Self::Message>();
+        let (_, r) = channel::<Self::Message>();
         let s = Rc::from(RefCell::from(self.clone()));
         utils::set_interval(30, move || {
             if let Some(msg) = r.recv() {
--- livid/src/document.rs
+++ livid/src/document.rs
@@ -1,125 +1,4 @@
 use wasm_bindgen::JsCast;
-use std::collections::hash_map::DefaultHasher;
-use std::hash::{Hash, Hasher};
-use std::{any, marker, mem, os::raw};
-
-static mut SENDER: Option<crossbeam_channel::Sender<*mut raw::c_void>> = None;
-static mut RECEIVER: Option<crossbeam_channel::Receiver<*mut raw::c_void>> = None;
-
-#[repr(C)]
-struct Message<T: Send + Sync> {
-    hash: u64,
-    sz: usize,
-    msg: T,
-}
-
-/// Creates a sender struct
-#[derive(Debug, Copy)]
-pub struct Sender<T: Send + Sync> {
-    data: marker::PhantomData<T>,
-    hash: u64,
-    sz: usize,
-}
-
-// Manually create the impl so there's no Clone bound on T
-impl<T: Send + Sync> Clone for Sender<T> {
-    fn clone(&self) -> Self {
-        Sender {
-            data: marker::PhantomData,
-            hash: self.hash,
-            sz: self.sz,
-        }
-    }
-}
-
-impl<T: Send + Sync> Sender<T> {
-    /// Sends a message
-    pub fn send(&self, val: T) {
-        let msg = Message {
-            hash: self.hash,
-            sz: self.sz,
-            msg: val,
-        };
-        unsafe {
-            if let Some(s) = &SENDER {
-                s.try_send(Box::into_raw(Box::from(msg)) as *mut raw::c_void)
-                    .ok();
-            }
-        }
-    }
-}
-
-/// Creates a receiver struct
-#[derive(Debug, Copy)]
-pub struct Receiver<T: Send + Sync> {
-    data: marker::PhantomData<T>,
-    hash: u64,
-    sz: usize,
-}
-//
-// Manually create the impl so there's no Clone bound on T
-impl<T: Send + Sync> Clone for Receiver<T> {
-    fn clone(&self) -> Self {
-        Receiver {
-            data: marker::PhantomData,
-            hash: self.hash,
-            sz: self.sz,
-        }
-    }
-}
-
-impl<T: Send + Sync> Receiver<T> {
-    /// Receives a message
-    pub fn recv(&self) -> Option<T> {
-        if let Some(r) = unsafe { &RECEIVER } {
-            if let Ok(msg) = r.try_recv() {
-                if !msg.is_null() {
-                    let data = unsafe { Box::from_raw(msg as *const _ as *mut Message<T>) };
-                    if data.sz == self.sz && data.hash == self.hash {
-                        Some(data.msg)
-                    } else {
-                        None
-                    }
-                } else {
-                    None
-                }
-            } else {
-                None
-            }
-        } else {
-            None
-        }
-    }
-}
-
-
-/// Creates a channel returning a Sender and Receiver structs (mpsc: multiple producer single consumer).
-pub fn channel<T: Send + Sync>() -> (Sender<T>, Receiver<T>) {
-    unsafe {
-        if SENDER.is_none() || RECEIVER.is_none() {
-            let (s, r) = crossbeam_channel::unbounded();
-            SENDER = Some(s);
-            RECEIVER = Some(r);
-        }
-    }
-    let msg_sz = mem::size_of::<T>();
-    let type_name = any::type_name::<T>();
-    let mut hasher = DefaultHasher::new();
-    type_name.hash(&mut hasher);
-    let type_hash = hasher.finish();
-
-    let s = Sender {
-        data: marker::PhantomData,
-        hash: type_hash,
-        sz: msg_sz,
-    };
-    let r = Receiver {
-        data: marker::PhantomData,
-        hash: type_hash,
-        sz: msg_sz,
-    };
-    (s, r)
-}
 
 /// Get the global window
 fn window() -> web_sys::Window {
@@ -152,8 +31,11 @@ impl Document {
 
     /// add a link
     pub fn add_css_link(href: &str) {
-        let link: web_sys::HtmlLinkElement =
-            Document::get().create_element("link").unwrap().dyn_into().unwrap();
+        let link: web_sys::HtmlLinkElement = Document::get()
+            .create_element("link")
+            .unwrap()
+            .dyn_into()
+            .unwrap();
         link.set_rel("stylesheet");
         link.set_type("text/css");
         link.set_href(href);
